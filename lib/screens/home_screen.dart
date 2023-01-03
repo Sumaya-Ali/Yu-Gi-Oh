cdb6e5f0e0692c70d72a25e6efec1af839ae24c8
SumayaAli <Sumaya.Jalal.Ali@gmail.com>
2023-01-03 22:07:13 +0200

    support arabic using google_fonts & flutter_localizations widgets

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:yu_gi_oh/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ePrimaryColor,
+      appBar: homeAppBar(),
+    );
+  }
+
+  AppBar homeAppBar() {
+    return AppBar(
+      elevation: 0,
+      title:Text(
+          "مرحبا بكم في أوراق يوغي يو",
+        style: GoogleFonts.getFont('Almarai'),),
+      centerTitle: false,
+      actions: [
+        IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
+      ],
+    );
+  }
+}
